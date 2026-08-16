e1f24d8db314aed9812a9df54067a8b3f06677af
pogJames <121344467+pogJames@users.noreply.github.com>
2026-08-16 19:04:08 +0800

    cool factor

--- script.js
+++ script.js
@@ -13,26 +13,3 @@ const observer = new IntersectionObserver((entries, observer) => {
 
 // Observe each box
 hidden_boxes.forEach(hidden_box => observer.observe(hidden_box));
-
-// Add button to switch light/dark mode
-const toggledark = document.getElementById("toggle-dark");
-const body = document.body;
-const moon = document.getElementById("moon");
-const sun = document.getElementById("sun");
-const theme = localStorage.getItem("pref-theme") || 'dark';
-
-if (theme === "light") {
-    body.classList.toggle("dark");
-    moon.style.display = "none";
-    sun.style.display = "inline";
-    localStorage.setItem("pref-theme", "light");
-}
-
-toggledark.addEventListener("click", () => {
-    // Toggle dark mode
-    body.classList.toggle("dark");
-    const isDark = document.body.classList.contains("dark");
-    moon.style.display = isDark ? "inline" : "none";
-    sun.style.display = isDark ? "none" : "inline";
-    localStorage.setItem("pref-theme", isDark ? "dark" : "light");
-});
